b74397a19c90009e9fcc9ad396f87f57c9341487
tiawl <pabtomas@protonmail.com>
2024-11-02 21:08:51 +0100

    [update] try to make it works for windows

--- build.zig
+++ build.zig
@@ -122,7 +122,7 @@ pub fn build (builder: *std.Build) !void
      .oniguruma = .{
        .name = "kkos/oniguruma",
        .host = toolbox.Repository.Host.github,
-       .ref = toolbox.Repository.Reference.commit,
+       .ref = toolbox.Repository.Reference.tag,
      },
    });
 
